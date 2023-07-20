3a6fcb87618b747e9851002494f5896ef7b5c64d
Walter Alarcon Garces <walarcon@gmail.com>
2023-07-20 18:37:14 -0400

    se agregó las cards y se importó bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "pages#home"
+  root 'pages#index'
+  get '/projects', to: 'projects#index'
+  get '/contact', to: 'contact#index'
+  
 end
